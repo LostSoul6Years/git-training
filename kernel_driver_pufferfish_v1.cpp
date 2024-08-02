5fd7aba7755c8cc3c1b09d801abdfd149830b669
StupidBrain <jjcdeveloper@gmail.com>
2024-08-03 00:08:52 +0800

    pufferfish update and successfully averted staging crisis

---
+++ kernel_driver_pufferfish_v1.cpp
@@ -0,0 +1,12 @@
+#include <linux.h> //just some pseudo headers 
+#include <kernel.h> //just some psuedo headers
+
+
+extern void (*asdada)(int, void); //LMA OITS A VARIABLE OFC I GET TO DECLARE IT EXTERN... ARE YOU STUPID OR STH BRO
+
+
+int main(int argc, char** argv){
+  //cope with pufferfish power.
+}
+
+
